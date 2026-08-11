07cbf21795893e120e70026386b4738733e34d2e
yushunsong <yushunsong@juneyaoair.com>
2026-08-11 17:45:46 +0800

    feat:update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'prisma/config';
 
-// 优先读环境变量，否则用兜底值
+// 优先读环境变量，否则用生产环境兜底值
 const databaseUrl =
   process.env.DATABASE_URL ||
-  'postgresql://common:common123@localhost:5432/account?schema=public&timezone=Asia%2FShanghai';
+  'postgresql://account:account@postgres:5432/account?schema=public&timezone=Asia%2FShanghai';
 
 const shadowDatabaseUrl =
   process.env.SHADOW_DATABASE_URL ||
